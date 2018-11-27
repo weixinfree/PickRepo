64689e6443f8a1190e642b7ce6668d204a37a00a
wangw <wangw@inke.cn>
2018-11-27 11:58:38 +0800

    first commit

---
+++ picrepo.py
@@ -0,0 +1,15 @@
+
+
+class PicRepo:
+
+    def __init__(self):
+        pass
+
+    def init(self):
+        pass
+
+    def add(self, name: str, url: str):
+        pass
+
+    def add_dir(self):
+        pass
